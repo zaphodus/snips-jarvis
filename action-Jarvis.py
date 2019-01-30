9c1257896aae24096ad97a00460998bcdfc87f1b
respeaker <respeaker@seeed.cc>
2019-01-30 19:33:55 +0100

    0

--- action-Jarvis.py
+++ action-Jarvis.py
@@ -10,31 +10,26 @@ CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
 class SnipsConfigParser(ConfigParser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
-
+	def to_dict(self):
+		return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
 
 def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
-        return dict()
+	try:
+		with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+			conf_parser = SnipsConfigParser()
+			conf_parser.readfp(f)
+			return conf_parser.to_dict()
+	except (IOError, ConfigParser.Error) as e:
+		return dict()
 
 def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
+	conf = read_configuration_file(CONFIG_INI)
+	action_wrapper(hermes, intentMessage, conf)
 
 
 def action_wrapper(hermes, intentMessage, conf):
-    hermes.publish_end_session(current_session_id, "I'm Jarvis! Just A Rather Very Intelligent System!")
-
-
-
+	hermes.publish_end_session(current_session_id, "I'm Jarvis! Just A Rather Very Intelligent System!")
 
 if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
-        h.subscribe_intent("kajdocsi:Jarvis", subscribe_intent_callback) \
-         .start()
+	with Hermes(conf['secret']['mqtt_host']+":"+conf['secret']['mqtt_port']) as h:
+		h.subscribe_intent("kajdocsi:Jarvis", subscribe_intent_callback).start()
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#/usr/bin/env bash -e
+
+VENV=venv
+
+if [ ! -e "./config.ini" ]; then
+	cp config.ini.default config.ini
+fi
+
+if [ ! -d "$VENV" ]; then
+	PYTHON=`which python3`
+	if [ ! -f $PYTHON ]; then
+		echo "could not find python"
+	fi
+	virtualenv -p $PYTHON $VENV
+fi
+
+. $VENV/bin/activate
+
+pip install -r requirements.txt
